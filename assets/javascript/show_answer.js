f36be3fec81e5081eb7ad1ed9348eec9beddcd0f
Marcel Epp <marcel.epp@tutanota.de>
2024-01-18 15:04:16 +0100

    add javascript for button

--- assets/javascript/show_answer.js
+++ assets/javascript/show_answer.js
@@ -4,5 +4,12 @@ const cardButtons = document.querySelectorAll('[data-js="card-button"]');
 cardButtons.forEach(function (cardButton) {
   cardButton.addEventListener("click", () => {
     cardButton.nextElementSibling.classList.toggle("card-answer-visible");
+    if (cardButton.innerHTML === "Show Answer") {
+      cardButton.innerHTML = "Close Answer";
+      cardButton.classList.add("card-button-open");
+    } else {
+      cardButton.innerHTML = "Show Answer";
+      cardButton.classList.remove("card-button-open");
+    }
   });
 });
